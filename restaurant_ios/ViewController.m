f6559580664b36049b9f5259adb53ad815f13fe8
村上智紀 <fankami@murakami.local>
2014-10-24 18:53:29 +0900

    Initial Commit

---
+++ restaurant_ios/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  restaurant_ios
+//
+//  Created by 村上智紀 on 2014/10/24.
+//  Copyright (c) 2014年 pad. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
